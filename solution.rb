f8af5567c6dab6234e5339b48b4b49fab436a62d
juandavidac <jd.ac@outlook.com>
2015-11-27 14:52:49 -0500

    simplificacion

--- solution.rb
+++ solution.rb
@@ -1,13 +1,9 @@
-require "sinatra"
+require 'sinatra'
 
 get '/' do
-  unless params[:permiso]
-    "Sin permiso"
-  else
       if params[:permiso]=="soy-un-token-secreto"
         "Si lo logramos!"
       else
         "Sin permiso"
       end
-  end
 end
